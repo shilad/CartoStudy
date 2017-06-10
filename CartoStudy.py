f1a9ee16bd596ac0a88ece2fd0bb085780588914
Shilad Sen <Shilad.Sen@target.com>
2017-06-10 15:36:35 -0500

    Remove cached versioning.

--- CartoStudy.py
+++ CartoStudy.py
@@ -259,29 +259,29 @@ def thanksSaved():
     return render_template('thanks_saved.html', code=code)
 
 
-@app.url_defaults
-def hashed_url_for_static_file(endpoint, values):
-    if 'static' == endpoint or endpoint.endswith('.static'):
-        filename = values.get('filename')
-        if filename:
-            if '.' in endpoint:  # has higher priority
-                blueprint = endpoint.rsplit('.', 1)[0]
-            else:
-                blueprint = request.blueprint  # can be None too
-
-            if blueprint:
-                static_folder = app.blueprints[blueprint].static_folder
-            else:
-                static_folder = app.static_folder
-
-            param_name = 'h'
-            while param_name in values:
-                param_name = '_' + param_name
-            values[param_name] = static_file_hash(os.path.join(static_folder, filename))
-
-
-def static_file_hash(filename):
-    return int(os.stat(filename).st_mtime)  # or app.config['last_build_timestamp'] or md5(filename) or etc...
+# @app.url_defaults
+# def hashed_url_for_static_file(endpoint, values):
+#     if 'static' == endpoint or endpoint.endswith('.static'):
+#         filename = values.get('filename')
+#         if filename:
+#             if '.' in endpoint:  # has higher priority
+#                 blueprint = endpoint.rsplit('.', 1)[0]
+#             else:
+#                 blueprint = request.blueprint  # can be None too
+#
+#             if blueprint:
+#                 static_folder = app.blueprints[blueprint].static_folder
+#             else:
+#                 static_folder = app.static_folder
+#
+#             param_name = 'h'
+#             while param_name in values:
+#                 param_name = '_' + param_name
+#             values[param_name] = static_file_hash(os.path.join(static_folder, filename))
+#
+#
+# def static_file_hash(filename):
+#     return int(os.stat(filename).st_mtime)  # or app.config['last_build_timestamp'] or md5(filename) or etc...
 
 if __name__ == '__main__':
     app.run()
